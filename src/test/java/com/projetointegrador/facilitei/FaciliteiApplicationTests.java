9b4eef5f2c2ae418515a65aae8b963a86ea1a7e2
Jorge <jarabelo68@gmail.com>
2025-05-16 08:03:49 -0300

    First Commit

---
+++ src/test/java/com/projetointegrador/facilitei/FaciliteiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.facilitei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaciliteiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
