00fd08cc4a3db78bba04d08b0d0ad16a5fb4aa7c
ricardoissadesousa <ricardoissadesousa@gmail.com>
2025-06-27 10:21:13 -0300

    finalizado o chat

---
+++ src/main/java/psg/facilitei/Controller/LiveChatController.java
@@ -0,0 +1,24 @@
+package psg.facilitei.Controller;
+
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
+import psg.facilitei.Controller.domain.ChatInput;
+import psg.facilitei.Controller.domain.ChatOutput;
+
+
+@Controller
+public class LiveChatController {
+
+    @MessageMapping("/new-message")
+    @SendTo("/topics/livechat")
+    public ChatOutput newMessage(ChatInput input){
+        return new ChatOutput(HtmlUtils.htmlEscape( input.user()+ ": "+input.message()));
+
+    }
+
+
+}
