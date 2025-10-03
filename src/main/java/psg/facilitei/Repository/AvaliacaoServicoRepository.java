5c4d06cac370cb3e0c9ae6381681a732b92a3712
LDRRosa <leandro.rosa.prof@gmail.com>
2025-10-03 09:06:00 -0300

    mudança na deleção da avaliação

--- src/main/java/psg/facilitei/Repository/AvaliacaoServicoRepository.java
+++ src/main/java/psg/facilitei/Repository/AvaliacaoServicoRepository.java
@@ -1,6 +1,8 @@
 package psg.facilitei.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import psg.facilitei.Entity.AvaliacaoServico;
 
@@ -12,4 +14,7 @@ public interface AvaliacaoServicoRepository extends JpaRepository<AvaliacaoServi
     List<AvaliacaoServico> findByServicoId(Long servicoId);
 
     List<AvaliacaoServico> findByClienteId(Long clienteId);
+
+    @Query("SELECT AVG(a.nota) FROM AvaliacaoServico a WHERE a.servico.trabalhador.id = :trabalhadorId")
+    Double calcularMediaPorTrabalhador(@Param("trabalhadorId") Long trabalhadorId);
 }
