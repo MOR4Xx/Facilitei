30a9d47f023ea931bd91477f3a3c4bc4334349b2
Arthur Estrela <arthurestrela369@gmail.com>
2025-11-19 11:54:09 -0300

    fix: ajustado caminho

--- src/main/java/psg/facilitei/Config/Origins.java
+++ src/main/java/psg/facilitei/Config/Origins.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class Origins implements WebMvcConfigurer {
 
-    @Value("${myapp.cors.allowed-origins:http://localhost:8080}")
+    @Value("${myapp.cors.allowed-origins:http://localhost:5173}")
     private String allowedOrigins;
 
     @Override
